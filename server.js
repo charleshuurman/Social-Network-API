975d2eec2ec19b2ca7ebc4e4483f133ef3cf7735
Charles Huurman <ch@utopian.ink>
2023-12-14 03:11:37 +0100

    “Fix_Server.js”

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes'); 
 
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the Social Network API!');
+});
+
 app.use('/api', userRoutes);
+app.use('/api', thoughtRoutes); 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDB', {
     useNewUrlParser: true,
