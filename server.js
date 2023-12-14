62dc16478fd5d624ca0f7d07c89dfa752df7a9bf
Charles Huurman <ch@utopian.ink>
2023-12-14 03:00:37 +0100

    “fix_PORT”

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
